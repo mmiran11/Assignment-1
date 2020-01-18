589c346783fc30bd26b5d816a5b8a3f2c3af7bea
mmiran11 <mmiran11@students.kennesaw.edu>
2020-01-18 14:29:19 -0500

    added constructors

--- Test.java
+++ Test.java
@@ -1,4 +1,17 @@
 
 public class Test {
-
+	String firstName, lastName;
+	int [] testScores = new int[5];
+	float avg;
+	
+	Test() {
+		this.firstName = "Bob";
+		this.lastName = "Jackson";
+	}
+	
+	Test (String firstName, String lastName, int [] arr){
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.testScores = arr;
+	}
 }
