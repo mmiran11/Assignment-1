b691a25ab3222d71dcea365c0f5c1c90353d2b3f
mmiran11 <mmiran11@students.kennesaw.edu>
2020-01-18 19:17:14 -0500

    Starting main program from the beginning

--- src/TestMain.java
+++ src/TestMain.java
@@ -3,14 +3,7 @@ public class TestMain {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int [] studentScores = new int [5];
-		for(int i = 0; i < studentScores.length; i++) {
-			System.out.print ("Please enter your score for test " + (i + 1) + ": ");
-			studentScores[i] = scan.nextInt();
-		}
-		Test Student1 = new Test("Sara", "Williams", studentScores);
-		int scoreAverage = Student1.averageScore(studentScores);
-		System.out.println (scoreAverage);
+		
 	}
 
 }
